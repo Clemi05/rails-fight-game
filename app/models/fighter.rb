d2397e41414049c69b475e806e61eb2e93786143
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-13 15:50:33 +0200

    added stats Win/lose index fighters

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,8 +1,8 @@
 class Fighter < ApplicationRecord
   has_one_attached :photo
 
-  has_many :victories, foreign_key: :winner_id
-  has_many :losses, foreign_key: :loser_id
+  has_many :victories, class_name: 'Fight', foreign_key: :winner_id
+  has_many :losses, class_name: 'Fight', foreign_key: :loser_id
 
   validates :name, length: { maximum: 15 }, uniqueness: true, presence: true
   validates :hp, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 250 }
